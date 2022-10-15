a1455bc32b8c0a1e1ab11471d542797793c64261
Matt Aitken <matt@mattaitken.com>
2022-10-15 17:09:23 +0100

    Created the user repository

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,12 @@
+import { Prisma } from "@prisma/client";
+
 export * from "./client";
 
 export type { PrismaClient } from "./client";
+
+//generic Prisma types
+type JsonObject = Prisma.JsonObject;
+export type { JsonObject };
 
 //schema types
 export type { User } from "@prisma/client";
