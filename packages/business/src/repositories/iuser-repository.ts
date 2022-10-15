25fe399035ad8a327834d75da6c883bd433ef5e7
Matt Aitken <matt@mattaitken.com>
2022-10-15 17:09:23 +0100

    Created the user repository

--- packages/business/src/repositories/iuser-repository.ts
+++ packages/business/src/repositories/iuser-repository.ts
@@ -1,6 +1,28 @@
 import type { User } from "../shared/user";
+import type { GitHubProfile } from "remix-auth-github";
+
+export type FindOrCreateMagicLink = {
+  authenticationMethod: "MAGIC_LINK";
+  email: string;
+};
+
+export type FindOrCreateGithub = {
+  authenticationMethod: "GITHUB";
+  email: User["email"];
+  accessToken: User["accessToken"];
+  authenticationProfile: GitHubProfile;
+  authenticationExtraParams: Record<string, unknown>;
+};
+
+export type FindOrCreateUser = FindOrCreateMagicLink | FindOrCreateGithub;
+
+export type LoggedInUser = {
+  user: User;
+  isNewUser: boolean;
+};
 
 export interface UserRepository {
-  getUsers(): Promise<User[]>;
-  getUsersCount(): Promise<number>;
+  asyncfindOrCreateUser(input: FindOrCreateUser): Promise<LoggedInUser>;
+  getUserById(id: User["id"]);
+  getUserByEmail(email: User["email"]);
 }
--- packages/business/src/repositories/user-repository.ts
+++ packages/business/src/repositories/user-repository.ts
@@ -1,16 +1,106 @@
 import { autoInjectable, inject } from "tsyringe";
-import type { PrismaClient } from "@remix-gospel-stack/database";
-import type { UserRepository } from "./iuser-repository";
+import type { JsonObject, PrismaClient } from "@remix-gospel-stack/database";
+
+import type {
+  FindOrCreateGithub,
+  FindOrCreateMagicLink,
+  FindOrCreateUser,
+  LoggedInUser,
+  UserRepository,
+} from "./iuser-repository";
 import type { User } from "../shared/user";
 
 @autoInjectable()
 export class PrismaUserRepository implements UserRepository {
   constructor(@inject("PrismaClient") private readonly prisma: PrismaClient) {}
-  async getUsers(): Promise<User[]> {
-    return this.prisma.user.findMany();
+
+  asyncfindOrCreateUser(input: FindOrCreateUser): Promise<LoggedInUser> {
+    switch (input.authenticationMethod) {
+      case "GITHUB": {
+        return this.findOrCreateGithubUser(input);
+      }
+      case "MAGIC_LINK": {
+        return this.findOrCreateMagicLinkUser(input);
+      }
+    }
   }
 
-  async getUsersCount(): Promise<number> {
-    return this.prisma.user.count();
+  private async findOrCreateMagicLinkUser(
+    input: FindOrCreateMagicLink
+  ): Promise<LoggedInUser> {
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email: input.email,
+      },
+    });
+
+    const user = await prisma.user.upsert({
+      where: {
+        email: input.email,
+      },
+      update: { email: input.email },
+      create: { email: input.email, authenticationMethod: "MAGIC_LINK" },
+    });
+
+    return {
+      user,
+      isNewUser: !existingUser,
+    };
+  }
+
+  private async findOrCreateGithubUser({
+    email,
+    accessToken,
+    authenticationProfile,
+    authenticationExtraParams,
+  }: FindOrCreateGithub): Promise<LoggedInUser> {
+    const name = authenticationProfile._json.name;
+    let avatarUrl: string | undefined = undefined;
+    if (authenticationProfile.photos[0]) {
+      avatarUrl = authenticationProfile.photos[0].value;
+    }
+    const displayName = authenticationProfile.displayName;
+    const authProfile = authenticationProfile
+      ? (authenticationProfile as unknown as JsonObject)
+      : undefined;
+    const authExtraParams = authenticationExtraParams
+      ? (authenticationExtraParams as unknown as JsonObject)
+      : undefined;
+
+    const fields = {
+      accessToken,
+      authenticationProfile: authProfile,
+      authenticationExtraParams: authExtraParams,
+      name,
+      avatarUrl,
+      displayName,
+    };
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    const user = await prisma.user.upsert({
+      where: {
+        email,
+      },
+      update: fields,
+      create: { ...fields, email, authenticationMethod: "GITHUB" },
+    });
+
+    return {
+      user,
+      isNewUser: !existingUser,
+    };
+  }
+
+  async getUserById(id: User["id"]) {
+    return prisma.user.findUnique({ where: { id } });
+  }
+
+  async getUserByEmail(email: User["email"]) {
+    return prisma.user.findUnique({ where: { email } });
   }
 }
