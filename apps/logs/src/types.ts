3e908b472f5ed97e559126e7b0b16e15a1dd8647
Matt Aitken <matt@mattaitken.com>
2022-11-03 12:08:20 +0000

    Zod types

---
+++ apps/logs/src/types.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+
+const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
+type Literal = z.infer<typeof literalSchema>;
+type Json = Literal | { [key: string]: Json } | Json[];
+const jsonSchema: z.ZodType<Json> = z.lazy(() =>
+  z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)])
+);
+
+const dateSchema = z.preprocess((arg) => {
+  if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
+}, z.date());
+
+const httpMethodSchema = z.union([
+  z.literal("GET"),
+  z.literal("POST"),
+  z.literal("PUT"),
+  z.literal("PATCH"),
+  z.literal("DELETE"),
+  z.literal("HEAD"),
+  z.literal("OPTIONS"),
+  z.literal("CONNECT"),
+  z.literal("TRACE"),
+]);
+
+const httpHeaderSchema = z.record(z.string());
+
+const logSchema = z.object({
+  id: z.string(),
+  projectId: z.string(),
+  method: httpMethodSchema,
+  statusCode: z.number(),
+  baseUrl: z.string(),
+  path: z.string(),
+  search: z.string(),
+  requestHeaders: httpHeaderSchema,
+  requestBody: jsonSchema.optional(),
+  responseHeaders: httpHeaderSchema,
+  responseBody: jsonSchema.optional(),
+  isCacheHit: z.boolean(),
+  responseSize: z.number(),
+  requestDuration: z.number(),
+  gatewayDuration: z.number(),
+  createdAt: dateSchema,
+});
+
+type Log = z.infer<typeof logSchema>;
