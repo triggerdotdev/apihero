cae91de0384f08625dcb1d0f8e083c77825c6e56
Matt Aitken <matt@mattaitken.com>
2022-11-03 16:14:48 +0000

    WIP on posting logs, with authentication working

--- apps/logs/src/routes/log/index.ts
+++ apps/logs/src/routes/log/index.ts
@@ -1,9 +1,18 @@
 import { FastifyPluginAsync } from "fastify";
+import invariant from "tiny-invariant";
+
+const logsToken = process.env.LOGS_API_AUTHENTICATION_TOKEN;
+invariant(logsToken, "LOGS_API_AUTHENTICATION_TOKEN is required");
 
 const log: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/", async function (request, reply) {
+  fastify.post("/", async function (request, reply) {
+    if (request.headers.authorization !== `Bearer ${logsToken}`) {
+      reply.status(401).send("Unauthorized");
+      return;
+    }
+
     const client = await fastify.pg.query("SELECT NOW()");
-    console.log(client);
+    // console.log(client);
 
     return "this is the logs endpoint";
   });
--- apps/logs/test/routes/log.test.ts
+++ apps/logs/test/routes/log.test.ts
@@ -1,12 +1,38 @@
 import { test } from "tap";
 import { build } from "../helper";
 
-test("log is loaded", async (t) => {
+test("create log fail without authentication", async (t) => {
   const app = await build(t);
 
   const res = await app.inject({
     url: "/log",
+    method: "POST",
+    headers: {},
   });
 
-  t.equal(res.payload, "this is a fucking example");
+  t.equal(res.statusCode, 401);
 });
+
+test("create log succeeds", async (t) => {
+  const app = await build(t);
+
+  const res = await app.inject({
+    url: "/log",
+    method: "POST",
+    headers: {
+      authorization: `Bearer ${process.env.LOGS_API_AUTHENTICATION_TOKEN}`,
+    },
+  });
+
+  t.equal(res.statusCode, 200);
+});
+
+// test("get logs", async (t) => {
+//   const app = await build(t);
+
+//   const res = await app.inject({
+//     url: "/log",
+//   });
+
+//   t.equal(res.payload, "this is a fucking example");
+// });
