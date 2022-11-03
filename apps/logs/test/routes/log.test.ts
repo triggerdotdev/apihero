0062147e396653264352f33b416602b72b7c5c66
Matt Aitken <matt@mattaitken.com>
2022-11-03 19:13:09 +0000

    Log getting created

--- apps/logs/test/routes/log.test.ts
+++ apps/logs/test/routes/log.test.ts
@@ -1,4 +1,5 @@
 import { test } from "tap";
+import { CreateLogBody } from "../../src/types";
 import { build } from "../helper";
 
 test("create log fail without authentication", async (t) => {
@@ -28,6 +29,44 @@ test("create log fails with invalid body", async (t) => {
   });
 
   t.equal(res.statusCode, 400);
+});
+
+test("create log succeeds with valid body", async (t) => {
+  const app = await build(t);
+
+  const body: CreateLogBody = {
+    projectId: "project-1",
+    method: "GET",
+    statusCode: 200,
+    baseUrl: "https://example.com",
+    path: "/",
+    search: "",
+    requestHeaders: {
+      "user-agent": "test",
+    },
+
+    responseHeaders: {
+      "content-type": "application/json",
+    },
+    responseBody: {
+      foo: "bar",
+    },
+    isCacheHit: false,
+    responseSize: 100,
+    requestDuration: 100,
+    gatewayDuration: 120,
+  };
+
+  const res = await app.inject({
+    url: "/log",
+    method: "POST",
+    headers: {
+      authorization: `Bearer ${process.env.LOGS_API_AUTHENTICATION_TOKEN}`,
+    },
+    body,
+  });
+
+  t.equal(res.statusCode, 200);
 });
 
 // test("get logs", async (t) => {
