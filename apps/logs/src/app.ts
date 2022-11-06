b215766cea4a4452823405b323f8b6a3d765811d
Matt Aitken <matt@mattaitken.com>
2022-11-06 13:23:15 +0000

    Brought back some of the plugin code

--- apps/logs/src/app.ts
+++ apps/logs/src/app.ts
@@ -11,17 +11,30 @@ import { CreateLogRequestBody, ErrorObject, Log } from "./types";
 import { databaseToLog } from "./utilities/log-conversion";
 import sensible from "@fastify/sensible";
 import { FastifyInstance } from "fastify";
+import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
+import { join } from "path";
 
 const databaseUrl = process.env.LOGS_DATABASE_URL;
 invariant(databaseUrl, "LOGS_DATABASE_URL is required");
 
-export default async function (app: FastifyInstance, options: any) {
+export type AppOptions = {
+  // Place your custom options for app below here.
+} & Partial<AutoloadPluginOptions>;
+
+// Pass --options via CLI arguments in command to enable these options.
+const options: AppOptions = {};
+
+export default async function (app: FastifyInstance, opts: AppOptions) {
   app.setValidatorCompiler(validatorCompiler);
   app.setSerializerCompiler(serializerCompiler);
 
-  app.register(sensible);
   app.register(postgres, {
     connectionString: process.env.LOGS_DATABASE_URL,
+  });
+
+  app.register(AutoLoad, {
+    dir: join(__dirname, "plugins"),
+    options: opts,
   });
 
   const logsToken = process.env.LOGS_API_AUTHENTICATION_TOKEN;
