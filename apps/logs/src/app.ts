9c49fba667d6d93da99ddcbbadc9ef6db227f2de
Matt Aitken <matt@mattaitken.com>
2022-11-06 12:47:25 +0000

    Switched to using a different zod plugin, which is working

--- apps/logs/src/app.ts
+++ apps/logs/src/app.ts
@@ -1,50 +1,95 @@
-import { join } from "path";
-import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
-import { FastifyPluginAsync } from "fastify";
+import Fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
 import invariant from "tiny-invariant";
 import postgres from "@fastify/postgres";
-import { buildJsonSchemas, register } from "fastify-zod";
-import { models } from "./types";
+import cuid from "cuid";
+import { z } from "zod";
+import { CreateLogRequestBody, ErrorObject, Log } from "./types";
+import { databaseToLog } from "./utilities/log-conversion";
+import sensible from "@fastify/sensible";
 
 const databaseUrl = process.env.LOGS_DATABASE_URL;
 invariant(databaseUrl, "LOGS_DATABASE_URL is required");
 
-export type AppOptions = {
-  // Place your custom options for app below here.
-} & Partial<AutoloadPluginOptions>;
-const options: AppOptions = {};
+const app = Fastify();
 
-const app: FastifyPluginAsync<AppOptions> = async (
-  fastify,
-  opts
-): Promise<void> => {
-  // Place here your custom code!
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-  void fastify.register(postgres, {
-    connectionString: process.env.LOGS_DATABASE_URL,
-  });
+app.register(sensible);
+app.register(postgres, {
+  connectionString: process.env.LOGS_DATABASE_URL,
+});
 
-  // Do not touch the following lines
+app.listen({ port: 3000 }, function (err) {
+  if (err) throw err;
+  app.log.info(`server listening on ${JSON.stringify(app.server.address())}`);
+});
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, "plugins"),
-    options: opts,
-  });
+const logsToken = process.env.LOGS_API_AUTHENTICATION_TOKEN;
+invariant(logsToken, "LOGS_API_AUTHENTICATION_TOKEN is required");
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, "routes"),
-    options: opts,
-  });
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "POST",
+  url: "/logs",
+  schema: {
+    headers: z.object({
+      authorization: z.string(),
+    }),
+    body: CreateLogRequestBody,
+    response: {
+      200: z.object({
+        success: z.literal(true),
+        log: Log,
+      }),
+      "4xx": ErrorObject,
+      "5xx": ErrorObject,
+    },
+  },
+  handler: async (request, reply) => {
+    if (request.headers.authorization !== `Bearer ${logsToken}`) {
+      reply.status(403).send({
+        statusCode: 403,
+        error: "Forbidden",
+        message: "Incorrect authorization",
+      });
+    }
 
-  fastify = await register(fastify, {
-    jsonSchemas: buildJsonSchemas(models),
-  });
-};
+    const id = cuid();
+    const query = `INSERT INTO "Log" (id, project_id, method, status_code, base_url, path, search, request_headers, request_body, response_headers, response_body, is_cache_hit, response_size, request_duration, gateway_duration) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *`;
 
-export default app;
-export { app, options };
+    const values = [
+      id,
+      request.body.projectId,
+      request.body.method,
+      request.body.statusCode,
+      request.body.baseUrl,
+      request.body.path,
+      request.body.search,
+      request.body.requestHeaders,
+      request.body.requestBody,
+      request.body.responseHeaders,
+      request.body.responseBody,
+      request.body.isCacheHit,
+      request.body.responseSize,
+      request.body.requestDuration,
+      request.body.gatewayDuration,
+    ];
+
+    try {
+      const queryResult = await app.pg.query(query, values);
+      const log = databaseToLog(queryResult.rows[0]);
+      reply.send({ success: true, log });
+    } catch (error) {
+      reply.status(500).send({
+        statusCode: 500,
+        error: "Internal error",
+        message: JSON.stringify(error),
+      });
+    }
+  },
+});
--- apps/logs/src/plugins/sensible.ts
+++ apps/logs/src/plugins/sensible.ts
@@ -1,5 +1,5 @@
-import fp from 'fastify-plugin'
-import sensible, { SensibleOptions } from '@fastify/sensible'
+import fp from "fastify-plugin";
+import sensible, { SensibleOptions } from "@fastify/sensible";
 
 /**
  * This plugins adds some utilities to handle http errors
@@ -7,5 +7,5 @@ import sensible, { SensibleOptions } from '@fastify/sensible'
  * @see https://github.com/fastify/fastify-sensible
  */
 export default fp<SensibleOptions>(async (fastify) => {
-  fastify.register(sensible)
-})
+  fastify.register(sensible);
+});
