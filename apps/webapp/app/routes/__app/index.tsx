1d557421d64012c39025e29d86fe911ebe15b6af
James Ritchie <james@jamesritchie.co.uk>
2022-11-07 13:57:50 +0000

    The route index redirects to a project if you have one

--- apps/webapp/app/routes/__app/index.tsx
+++ apps/webapp/app/routes/__app/index.tsx
@@ -1,16 +1,19 @@
-import { useUserWorkspacesData } from "~/routes/__app";
+import type { LoaderArgs } from "@remix-run/server-runtime";
+import { redirect } from "remix-typedjson";
+import { requireUserId } from "~/services/session.server";
+import { getWorkspaces } from "~/models/workspace.server";
 
-export default function IndexRoute() {
-  const { workspaces } = useUserWorkspacesData();
-  const hasProjects = workspaces.some((workspace) => workspace.projects.length);
-
-  return (
-    <div className="flex flex-shrink flex-grow items-center justify-between bg-slate-50">
-      {hasProjects ? (
-        <div className="flex h-full w-full">App route with project</div>
-      ) : (
-        <>App route without project</>
-      )}
-    </div>
+export const loader = async ({ request }: LoaderArgs) => {
+  const userId = await requireUserId(request);
+  const workspaces = await getWorkspaces({ userId });
+  const workspaceWithProject = workspaces.find(
+    (workspace) => workspace.projects.length
   );
-}
+
+  if (workspaceWithProject) {
+    return redirect(
+      `/workspaces/${workspaceWithProject.slug}/projects/${workspaceWithProject.projects[0].slug}/home`
+    );
+  }
+  return redirect(`/workspaces/new`);
+};
