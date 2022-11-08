98b5a4557335930eb79f5a37589f5a31cfd25293
Matt Aitken <matt@mattaitken.com>
2022-11-08 11:59:35 +0000

    Created logs healthcheck (connects to database)

---
+++ apps/logs/src/routes/healthcheck/healthcheck.ts
@@ -0,0 +1,36 @@
+import { FastifyPluginAsync } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { Client } from "pg";
+import { z } from "zod";
+
+const healthcheck: FastifyPluginAsync = async (app, opts): Promise<void> => {
+  app.withTypeProvider<ZodTypeProvider>().route({
+    method: "GET",
+    url: "/",
+    schema: {
+      headers: z.object({
+        host: z.string().optional(),
+      }),
+    },
+    handler: async (request, reply) => {
+      const host = request.headers["X-Forwarded-Host"] ?? request.headers.host;
+
+      try {
+        // if we can connect to the database and make a simple query
+        const client = new Client({
+          connectionString: process.env.DATABASE_URL,
+        });
+
+        await client.connect();
+        await client.query("SELECT 1");
+
+        reply.status(200).send("OK");
+      } catch (error: unknown) {
+        console.log("healthcheck ❌", { error });
+        reply.status(500).send("ERROR");
+      }
+    },
+  });
+};
+
+export default healthcheck;
