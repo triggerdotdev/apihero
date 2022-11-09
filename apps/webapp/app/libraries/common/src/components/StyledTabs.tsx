f36c977c2508b1ad3c6a365eeb49e2b9f5b4de96
Matt Aitken <matt@mattaitken.com>
2022-11-09 18:12:46 +0000

    Some components extracted out

--- apps/webapp/app/libraries/common/src/components/StyledTabs.tsx
+++ apps/webapp/app/libraries/common/src/components/StyledTabs.tsx
@@ -1,18 +1,21 @@
 import { Tab as HeadlessTab } from "@headlessui/react";
 import classnames from "classnames";
 
-export function ClassicList({ children }: { children: React.ReactNode }) {
+type HeadlessTabProps = Parameters<typeof HeadlessTab>[0];
+type HeadlessTabListProps = Parameters<typeof HeadlessTab.List>[0];
+
+export function ClassicList({ children, ...props }: HeadlessTabListProps) {
   return (
-    <HeadlessTab.List className={"-mb-px flex bg-slate-50"}>
+    <HeadlessTab.List className={"-mb-px flex bg-slate-50"} {...props}>
       {children}
     </HeadlessTab.List>
   );
 }
 
-export function Classic({ children }: { children: React.ReactNode }) {
+export function Classic({ children, ...props }: HeadlessTabProps) {
   return (
     <HeadlessTab
-      className={({ selected }) =>
+      className={({ selected }: { selected: boolean }) =>
         classnames(
           selected
             ? " border-slate-200 bg-white text-slate-600"
@@ -20,31 +23,28 @@ export function Classic({ children }: { children: React.ReactNode }) {
           "flex whitespace-nowrap border-r py-3 px-3 text-xs focus:outline-none"
         )
       }
+      {...props}
     >
       {children}
     </HeadlessTab>
   );
 }
 
-export function UnderlinedList({ children }: { children: React.ReactNode }) {
+export function UnderlinedList({ children, ...props }: HeadlessTabListProps) {
   return (
     <HeadlessTab.List
       className={"-mb-px flex space-x-4 border-b border-slate-200"}
+      {...props}
     >
       {children}
     </HeadlessTab.List>
   );
 }
 
-type UnderlineProps = {
-  children: React.ReactNode;
-  disabled?: boolean;
-};
-
-export function Underlined({ children, disabled = false }: UnderlineProps) {
+export function Underlined({ children, ...props }: HeadlessTabProps) {
   return (
     <HeadlessTab
-      className={({ selected }) =>
+      className={({ selected }: { selected: boolean }) =>
         classnames(
           selected
             ? "border-blue-500 text-slate-900 outline-none"
@@ -53,7 +53,7 @@ export function Underlined({ children, disabled = false }: UnderlineProps) {
           "flex whitespace-nowrap border-b-2 py-2 px-4 text-xs font-medium"
         )
       }
-      disabled={disabled}
+      {...props}
     >
       {children}
     </HeadlessTab>
