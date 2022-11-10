53132d67e18fd78608cfbd920675b7bf203f126c
James Ritchie <james@jamesritchie.co.uk>
2022-11-10 14:35:04 +0000

    Swapped out the dropdown menu for a Headless Popover instead

--- apps/webapp/app/libraries/ui/src/components/WorkspaceMenu.tsx
+++ apps/webapp/app/libraries/ui/src/components/WorkspaceMenu.tsx
@@ -1,20 +1,13 @@
-import { Combobox as HeadlessComboBox } from "@headlessui/react";
+import { Popover, Transition } from "@headlessui/react";
 import classNames from "classnames";
-import {
-  StyledInput,
-  StyledButton,
-  StyledChevron,
-  StyledOptions,
-  StyledOption,
-} from "./ComboBox";
-import { CheckIcon } from "@heroicons/react/24/solid";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
 import {
   BookmarkIcon,
   BriefcaseIcon,
   PlusIcon,
 } from "@heroicons/react/24/outline";
 import type { Workspace, Project } from ".prisma/client";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "@remix-run/react";
 
 type WorkspaceMenuProps = {
@@ -30,97 +23,109 @@ export default function WorkspaceMenu({
 }: WorkspaceMenuProps) {
   const [projectSlug, setProjectSlug] = useState(currentProjectSlug);
   return (
-    <HeadlessComboBox
-      value={projectSlug}
-      onChange={(v) => {
-        setProjectSlug(v);
-        //   if (onChange !== undefined) {
-        //     onChange(v);
-        //   }
-      }}
-    >
-      <div className="relative">
-        <StyledInput
-          onChange={(event) => {}}
-          displayValue={(selected: string) => selected}
-        />
-        <StyledButton>
-          <StyledChevron />
-        </StyledButton>
-        <StyledOptions>
-          {workspaces.map((workspace) => {
-            return (
-              <React.Fragment key={workspace.slug}>
-                <div className="flex items-center gap-2 mb-2">
-                  <BriefcaseIcon className="h-5 w-5 text-slate-600" />
-                  <span className="font-semibold text-base text-slate-600">
-                    {workspace.title}
-                  </span>
-                </div>
-                {workspace.projects.map((project) => {
-                  return (
-                    <div key={project.slug} className="flex flex-col gap-1">
-                      <StyledOption
-                        value={project.slug}
-                        to={`/workspaces/${workspace.slug}/projects/${project.slug}/home`}
-                      >
-                        {({ active, selected }) => (
-                          <>
-                            <div
-                              className={classNames(
-                                "flex items-center gap-1.5",
-                                selected && ""
-                              )}
-                            >
-                              <BookmarkIcon
-                                className="h-5 w-5"
+    <div className="w-full max-w-max px-4">
+      <Popover className="relative">
+        {({ open }) => (
+          <>
+            <Popover.Button
+              className={`
+                ${open ? "" : "text-opacity-90"}
+                group inline-flex items-center rounded-md bg-white px-3 py-2 text-sm text-slate-600 border border-slate-200 shadow hover:bg-slate-50 transition focus:outline-none`}
+            >
+              <span>James' Project</span>
+              <ChevronDownIcon
+                className={`${open ? "rotate-180" : "text-opacity-70"}
+                  ml-2 h-5 w-5 text-slate-600 transition duration-150 ease-in-out group-hover:text-opacity-80`}
+                aria-hidden="true"
+              />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen min-w-max max-w-xs -translate-x-1/2 transform px-4 sm:px-0">
+                <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                  <div className="relative grid py-1 bg-white grid-cols-1">
+                    {workspaces.map((workspace) => {
+                      return (
+                        <React.Fragment key={workspace.slug}>
+                          <div className="flex items-center gap-2 ml-2 mt-1 mb-2 py-0.5">
+                            <BriefcaseIcon className="h-5 w-5 text-slate-600" />
+                            <span className="font-semibold text-base text-slate-600">
+                              {workspace.title}
+                            </span>
+                          </div>
+                          {workspace.projects.map((project) => {
+                            return (
+                              <div
+                                key={project.slug}
+                                className="flex flex-col gap-1"
+                              >
+                                <Link
+                                  to={`/workspaces/${workspace.slug}/projects/${project.slug}/home`}
+                                  className={classNames(
+                                    "flex items-center justify-between gap-1.5 mx-1 px-3 py-2 text-slate-600 rounded hover:bg-slate-100 transition",
+                                    projectSlug === currentProjectSlug &&
+                                      "!bg-slate-200"
+                                  )}
+                                >
+                                  <div className="flex items-center gap-2">
+                                    <BookmarkIcon
+                                      className="h-5 w-5 z-100"
+                                      aria-hidden="true"
+                                    />
+                                    <span className="block truncate">
+                                      {project.title}
+                                    </span>
+                                  </div>
+                                  {projectSlug === currentProjectSlug && (
+                                    <CheckIcon className="h-5 w-5 text-blue-500" />
+                                  )}
+                                </Link>
+
+                                <Link
+                                  to={`/workspaces/${workspace.slug}/projects/new`}
+                                >
+                                  <div className="flex items-center gap-2 mx-1 pl-3 py-2 rounded bg-white hover:bg-slate-100 transition">
+                                    <PlusIcon
+                                      className="h-5 w-5 text-green-500"
+                                      aria-hidden="true"
+                                    />
+                                    <span className="text-slate-600">
+                                      New Project
+                                    </span>
+                                  </div>
+                                </Link>
+                                <div className="border-t w-full border-slate-100" />
+                              </div>
+                            );
+                          })}
+                          <Link to={`/workspaces/new`}>
+                            <div className="flex items-center gap-2 mx-1 mt-1 pl-1 py-2 rounded bg-white hover:bg-slate-100 transition">
+                              <PlusIcon
+                                className="h-5 w-5 text-green-500"
                                 aria-hidden="true"
                               />
-                              <span className="block truncate">
-                                {project.title}
+                              <span className="text-slate-600">
+                                New Workspace
                               </span>
                             </div>
-
-                            {selected && (
-                              <span
-                                className={classNames(
-                                  "absolute inset-y-0 right-0 flex items-center pr-4 rounded-r",
-                                  active ? "text-blue-500" : "text-blue-500"
-                                )}
-                              >
-                                <CheckIcon
-                                  className="h-5 w-5"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            )}
-                          </>
-                        )}
-                      </StyledOption>
-                      <Link to={`/workspaces/${workspace.slug}/projects/new`}>
-                        <div className="flex gap-2 pl-3 py-2 rounded bg-white hover:bg-slate-100 transition">
-                          <PlusIcon
-                            className="h-5 w-5 text-green-500"
-                            aria-hidden="true"
-                          />
-                          <span className="text-slate-600">New Project</span>
-                        </div>
-                      </Link>
-                    </div>
-                  );
-                })}
-              </React.Fragment>
-            );
-          })}
-          <Link to={`/workspaces/new`}>
-            <div className="border-t w-full mt-0.5 mb-0.5 border-slate-100" />
-            <div className="flex gap-2 pl-1 py-2 rounded bg-white hover:bg-slate-100 transition">
-              <PlusIcon className="h-5 w-5 text-green-500" aria-hidden="true" />
-              <span className="text-slate-600">New Workspace</span>
-            </div>
-          </Link>
-        </StyledOptions>
-      </div>
-    </HeadlessComboBox>
+                          </Link>
+                        </React.Fragment>
+                      );
+                    })}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </div>
   );
 }
