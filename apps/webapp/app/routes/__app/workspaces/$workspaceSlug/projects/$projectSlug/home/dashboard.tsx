a2559b43ae365edc9e092fb825095d032c797627
Matt Aitken <matt@mattaitken.com>
2022-11-10 18:05:29 +0000

    Added log filters to the dashboard

--- apps/webapp/app/routes/__app/workspaces/$workspaceSlug/projects/$projectSlug/home/dashboard.tsx
+++ apps/webapp/app/routes/__app/workspaces/$workspaceSlug/projects/$projectSlug/home/dashboard.tsx
@@ -1,8 +1,12 @@
+import { useLogs } from "~/libraries/common/src/hooks/useLogs";
+import { LogsFilters } from "~/libraries/ui/src/components/LogsFilters";
 import { LogsTabs } from "~/libraries/ui/src/components/LogsTabs";
 
 export default function Dashboard() {
+  const logs = useLogs();
   return (
     <div>
+      {logs && <LogsFilters logs={logs} />}
       <LogsTabs selected={"dashboard"} />
       Dashboard Index
     </div>
