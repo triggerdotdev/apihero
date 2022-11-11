8d81aace8d222c01814d8129408e396b3a724b07
Matt Aitken <matt@mattaitken.com>
2022-11-11 15:36:15 +0000

    Create new workspace with project

--- apps/webapp/app/routes/__app/workspaces/new.tsx
+++ apps/webapp/app/routes/__app/workspaces/new.tsx
@@ -4,15 +4,18 @@ import { json, redirect } from "@remix-run/node";
 import { Form, useActionData } from "@remix-run/react";
 import * as React from "react";
 import {
+  PrimaryButton,
   PrimaryLink,
   SecondaryLink,
 } from "~/libraries/ui/src/components/Buttons/Buttons";
+import { createProject } from "~/models/project.server";
 import { createWorkspace } from "~/models/workspace.server";
 import { requireUserId } from "~/services/session.server";
 
 type ActionData = {
   errors?: {
     title?: string;
+    projectTitle?: string;
     body?: string;
   };
 };
@@ -22,25 +25,46 @@ export const action: ActionFunction = async ({ request }) => {
 
   const formData = await request.formData();
   const title = formData.get("title");
-
   if (typeof title !== "string" || title.length === 0) {
     return json<ActionData>(
       { errors: { title: "Title is required" } },
       { status: 400 }
     );
   }
+  const projectTitle = formData.get("projectTitle");
+  if (typeof projectTitle !== "string" || projectTitle.length === 0) {
+    return json<ActionData>(
+      { errors: { projectTitle: "Project title is required" } },
+      { status: 400 }
+    );
+  }
 
-  const workspace = await createWorkspace({ title, userId });
-  return redirect(`/workspaces/${workspace.id}`);
+  try {
+    const workspace = await createWorkspace({ title, userId });
+    const project = await createProject({
+      title: projectTitle,
+      workspaceId: workspace.id,
+    });
+    return redirect(`/workspaces/${workspace.slug}/projects/${project.slug}`);
+  } catch (error: any) {
+    return json<ActionData>(
+      { errors: { body: error.message } },
+      { status: 400 }
+    );
+  }
 };
 
 export default function NewWorkspacePage() {
   const actionData = useActionData() as ActionData;
   const titleRef = React.useRef<HTMLInputElement>(null);
+  const projectTitleRef = React.useRef<HTMLInputElement>(null);
 
   React.useEffect(() => {
     if (actionData?.errors?.title) {
       titleRef.current?.focus();
+    }
+    if (actionData?.errors?.projectTitle) {
+      projectTitleRef.current?.focus();
     }
   }, [actionData]);
 
@@ -98,20 +122,24 @@ export default function NewWorkspacePage() {
                   <BookmarkIcon className="h-5 w-5 text-slate-600"></BookmarkIcon>
                 </div>
                 <input
-                  ref={titleRef}
-                  name="title"
+                  ref={projectTitleRef}
+                  name="projectTitle"
                   placeholder="e.g. My first Project"
                   className="relative w-full pl-10 pr-3 py-2 rounded-md border text-slate-600 bg-slate-50 group-focus:border-blue-500 placeholder:text-slate-400"
-                  aria-invalid={actionData?.errors?.title ? true : undefined}
+                  aria-invalid={
+                    actionData?.errors?.projectTitle ? true : undefined
+                  }
                   aria-errormessage={
-                    actionData?.errors?.title ? "title-error" : undefined
+                    actionData?.errors?.projectTitle
+                      ? "project-title-error"
+                      : undefined
                   }
                 />
               </div>
             </div>
-            {actionData?.errors?.title && (
-              <div className="pt-1 text-red-700" id="title-error">
-                {actionData.errors.title}
+            {actionData?.errors?.projectTitle && (
+              <div className="pt-1 text-red-700" id="project-title-error">
+                {actionData.errors.projectTitle}
               </div>
             )}
           </div>
@@ -124,13 +152,12 @@ export default function NewWorkspacePage() {
             >
               Cancel
             </SecondaryLink>
-            <PrimaryLink
-              to="/"
+            <PrimaryButton
               type="submit"
               className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
             >
               Create
-            </PrimaryLink>
+            </PrimaryButton>
           </div>
         </Form>
       </div>
