58da2cb1d347800db41e1f7bb4efe0b112b65c42
James Ritchie <james@jamesritchie.co.uk>
2022-11-14 16:40:07 +0000

    Added a working grid example as a test for the logs page layout

--- apps/webapp/app/routes/__app/workspaces/$workspaceSlug/projects/$projectSlug/home/logs.tsx
+++ apps/webapp/app/routes/__app/workspaces/$workspaceSlug/projects/$projectSlug/home/logs.tsx
@@ -85,62 +85,97 @@ export default function Logs() {
   return (
     <>
       {logs && (
-        <div className="grid grid-rows-[auto_1fr]">
-          <div className="bg-yellow-100">
-            <LogsFilters logs={logs} />
-            <LogsTabs selected={"logs"} />
-          </div>
-          <div className="grid grid-cols-[1fr_600px] bg-orange-50 max-h-full overflow-hidden">
-            <div className="overflow-hidden">
-              <div className="flex items-center justify-between pr-2 py-[5px] bg-slate-50">
-                <div className="flex items-center">
-                  <PreviousButton
-                    disabled={
-                      logs.previous !== undefined
-                        ? transition.state === "loading" ||
-                          transition.state === "submitting"
-                        : true
-                    }
-                    onClick={() => loadMore("previous")}
-                  />
+        // <div className="grid grid-rows-[auto_1fr]">
+        //   <div className="bg-yellow-100">
+        //     <LogsFilters logs={logs} />
+        //     <LogsTabs selected={"logs"} />
+        //   </div>
+        //   <div className="grid grid-cols-[1fr_600px] bg-orange-50 max-h-full overflow-hidden">
+        //     <div className="overflow-hidden">
+        //       <div className="flex items-center justify-between pr-2 py-[5px] bg-slate-50">
+        //         <div className="flex items-center">
+        //           <PreviousButton
+        //             disabled={
+        //               logs.previous !== undefined
+        //                 ? transition.state === "loading" ||
+        //                   transition.state === "submitting"
+        //                 : true
+        //             }
+        //             onClick={() => loadMore("previous")}
+        //           />
 
-                  <NextButton
-                    disabled={
-                      logs.next !== undefined
-                        ? transition.state === "loading" ||
-                          transition.state === "submitting"
-                        : true
-                    }
-                    onClick={() => loadMore("next")}
-                  />
-                </div>
+        //           <NextButton
+        //             disabled={
+        //               logs.next !== undefined
+        //                 ? transition.state === "loading" ||
+        //                   transition.state === "submitting"
+        //                 : true
+        //             }
+        //             onClick={() => loadMore("next")}
+        //           />
+        //         </div>
 
-                <RefreshButton
-                  disabled={false}
-                  onClick={() => reload()}
-                  lastUpdated={new Date()}
-                />
+        //         <RefreshButton
+        //           disabled={false}
+        //           onClick={() => reload()}
+        //           lastUpdated={new Date()}
+        //         />
+        //       </div>
+        //       <div className="overflow-scroll bg-green-100">
+        //         <LogsTable
+        //           logs={logs.logs}
+        //           selectedLogId={selectedLog ?? undefined}
+        //           onSelected={onLogSelected}
+        //         />
+        //       </div>
+        //     </div>
+        //     <Resizable
+        //       position="right"
+        //       initialSize={600}
+        //       minimumSize={270}
+        //       maximumSize={950}
+        //     >
+        //       <LogViewer
+        //         log={openLog}
+        //         position={position}
+        //         setPosition={setPosition}
+        //       />
+        //     </Resizable>
+        //   </div>
+        // </div>
+
+        <div className="main-container h-full grid grid-cols-[3fr_1fr] bg-red-100">
+          <div className="col-1 grid overflow-auto grid-rows-[1fr_1fr]">
+            <div className="row-1 grid grid-rows-[1fr_2fr] overflow-auto bg-green-100">
+              <div className="row-1-1 overflow-auto bg-blue-100">
+                <div className="test-height">row-1-1</div>
               </div>
-              <div className="overflow-scroll bg-green-100">
-                <LogsTable
-                  logs={logs.logs}
-                  selectedLogId={selectedLog ?? undefined}
-                  onSelected={onLogSelected}
-                />
+              <div className="row-1-2 overflow-auto bg-yellow-100">
+                <div className="test-height h-[1000px]">row-1-2</div>
               </div>
             </div>
-            <Resizable
-              position="right"
-              initialSize={600}
-              minimumSize={270}
-              maximumSize={950}
-            >
-              <LogViewer
-                log={openLog}
-                position={position}
-                setPosition={setPosition}
-              />
-            </Resizable>
+            <div className="row-2 bg-orange-100 overflow-auto grid grid-rows-[3fr_1fr]">
+              <div className="row-2-1 overflow-auto bg-lime-100">
+                <div className="test-height h-[1000px]">row-2-1</div>
+              </div>
+              <div className="row-2-2 overflow-auto bg-indigo-100">
+                <div className="test-height h-[1000px]">row-2-2</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-2 grid grid-rows-[90px_1fr_90px] overflow-auto">
+            <div className="row-3 bg-emerald-100"></div>
+            <div className="row-4 grid grid-rows-[1fr] overflow-auto bg-stone-200">
+              <div className="row-4-1 grid grid-rows-[1fr] overflow-auto p-5">
+                <div className="text-editor grid grid-rows-[auto_1fr] overflow-auto">
+                  <div className="toolbar bg-black text-white">Toolbar</div>
+                  <div className="content overflow-auto bg-amber-200">
+                    <div className="test-height h-[1000px]">Text content</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row-5 bg-red-100"></div>
           </div>
         </div>
       )}
