c8d8cec1aa10630486bc6fdbbc6d22307b441116
James Ritchie <james@jamesritchie.co.uk>
2022-11-16 15:26:26 +0000

    Made Label component useable without needing an Input

--- apps/webapp/app/libraries/ui/src/components/Primitives/Label.tsx
+++ apps/webapp/app/libraries/ui/src/components/Primitives/Label.tsx
@@ -1,4 +1,4 @@
-export function Label({ label, htmlFor }: { label: string; htmlFor: string }) {
+export function Label({ label, htmlFor }: { label: string; htmlFor?: string }) {
   return (
     <label
       className="block text-xs uppercase font-medium text-slate-500 tracking-wide"
