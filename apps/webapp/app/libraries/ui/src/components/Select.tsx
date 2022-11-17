937b3f2febc56dae36a8f939959d99c1dd4ffce5
James Ritchie <james@jamesritchie.co.uk>
2022-11-17 17:13:06 +0000

    Made the select menu dropdown smaller

--- apps/webapp/app/libraries/ui/src/components/Select.tsx
+++ apps/webapp/app/libraries/ui/src/components/Select.tsx
@@ -6,7 +6,7 @@ type SelectProps = React.DetailedHTMLProps<
 >;
 
 const defaultClasses =
-  "mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm";
+  "mt-1 block w-full rounded-md border-gray-300 py-1 pl-2 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm";
 
 export function Select({ children, className, ...props }: SelectProps) {
   return (
