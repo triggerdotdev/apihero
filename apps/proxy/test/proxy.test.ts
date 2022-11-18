8dd57c174be7db80a7933a143da1ab448010404b
Matt Aitken <matt@mattaitken.com>
2022-11-18 12:12:44 +0000

    Added GET and HEAD tests where a body is passed

--- apps/proxy/test/proxy.test.ts
+++ apps/proxy/test/proxy.test.ts
@@ -77,4 +77,68 @@ describe("proxy", () => {
     expect((json as any).url).toBe("https://httpbin.org/post");
     expect((json as any).json).toStrictEqual({ hello: "world" });
   });
+
+  test("Proxying GET requests with a body", async () => {
+    const request = new Request(`http://localhost/get`, {
+      method: "GET",
+      headers: {
+        [DESTINATION_HEADER_NAME]: "httpbin.org",
+        [PROTOCOL_HEADER_NAME]: "https",
+        [PROJECT_KEY_HEADER_NAME]: "hero_123",
+        [PAYLOAD_HEADER_NAME]: JSON.stringify({ env: "test" }),
+        host: "localhost",
+        "cf-connecting-ip": "127.0.0.1",
+        "cf-ipcountry": "US",
+        "cf-ray": "123",
+        "cf-visitor": '{"scheme":"https"}',
+      },
+    });
+
+    const [req, res] = await proxyRequest(request, { hello: "world" });
+    expect(req.url).toBe("https://httpbin.org/get");
+    expect(res.status).toBe(200);
+    expect(res.ok).toBe(true);
+    expect(req.headers.get(DESTINATION_HEADER_NAME)).toBeNull();
+    expect(req.headers.get(PROTOCOL_HEADER_NAME)).toBeNull();
+    expect(req.headers.get(PROJECT_KEY_HEADER_NAME)).toBeNull();
+    expect(req.headers.get(PAYLOAD_HEADER_NAME)).toBeNull();
+    expect(req.headers.get("host")).toBeNull();
+    expect(req.headers.get("cf-connecting-ip")).toBeNull();
+    expect(req.headers.get("cf-ipcountry")).toBeNull();
+    expect(req.headers.get("cf-ray")).toBeNull();
+    expect(req.headers.get("cf-visitor")).toBeNull();
+    const json = await res.json();
+    expect((json as any).url).toBe("https://httpbin.org/get");
+  });
+
+  test("Proxying HEAD requests with a body", async () => {
+    const request = new Request(`http://localhost/get`, {
+      method: "HEAD",
+      headers: {
+        [DESTINATION_HEADER_NAME]: "httpbin.org",
+        [PROTOCOL_HEADER_NAME]: "https",
+        [PROJECT_KEY_HEADER_NAME]: "hero_123",
+        [PAYLOAD_HEADER_NAME]: JSON.stringify({ env: "test" }),
+        host: "localhost",
+        "cf-connecting-ip": "127.0.0.1",
+        "cf-ipcountry": "US",
+        "cf-ray": "123",
+        "cf-visitor": '{"scheme":"https"}',
+      },
+    });
+
+    const [req, res] = await proxyRequest(request, { hello: "world" });
+    expect(req.url).toBe("https://httpbin.org/get");
+    expect(res.status).toBe(200);
+    expect(res.ok).toBe(true);
+    expect(req.headers.get(DESTINATION_HEADER_NAME)).toBeNull();
+    expect(req.headers.get(PROTOCOL_HEADER_NAME)).toBeNull();
+    expect(req.headers.get(PROJECT_KEY_HEADER_NAME)).toBeNull();
+    expect(req.headers.get(PAYLOAD_HEADER_NAME)).toBeNull();
+    expect(req.headers.get("host")).toBeNull();
+    expect(req.headers.get("cf-connecting-ip")).toBeNull();
+    expect(req.headers.get("cf-ipcountry")).toBeNull();
+    expect(req.headers.get("cf-ray")).toBeNull();
+    expect(req.headers.get("cf-visitor")).toBeNull();
+  });
 });
