b88c84ea89b9e9a0b2f09f468e97faab48309eab
Eric Allam <eallam@icloud.com>
2022-11-22 10:33:54 +0000

    Rename apihero-js to @apihero/browser

--- packages/apihero-browser/tsup.config.ts
+++ packages/apihero-browser/tsup.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "tsup";
 import { workerScriptPlugin } from "./config/plugins/esbuild/workerScriptPlugin";
 
-// Prevent from bundling the "@apihero/*" packages
-// so that the users get the latest versions without
-// having to bump them in "apihero-js'."
-const ecosystemDependencies = /^@apihero\/(.+)$/;
-
 export default defineConfig([
   {
     name: "main",
@@ -31,27 +26,6 @@ export default defineConfig([
     bundle: true,
     sourcemap: true,
     splitting: false,
-    dts: true,
-    esbuildPlugins: [workerScriptPlugin()],
-  },
-  {
-    name: "node",
-    entry: ["./src/node/index.ts"],
-    format: ["esm", "cjs"],
-    outDir: "./lib/node",
-    platform: "node",
-    external: [
-      "http",
-      "https",
-      "util",
-      "events",
-      "tty",
-      "os",
-      "timers",
-      ecosystemDependencies,
-    ],
-    clean: true,
-    sourcemap: true,
     dts: true,
     esbuildPlugins: [workerScriptPlugin()],
   },
