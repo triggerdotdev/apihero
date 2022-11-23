7562ea440221b90eae0e1826dfa83d5f096022c2
Eric Allam <eallam@icloud.com>
2022-11-23 13:47:33 +0000

    Turn API Hero back on

--- apps/webapp/app/entry.server.tsx
+++ apps/webapp/app/entry.server.tsx
@@ -1,4 +1,4 @@
-// import * as apihero from "~/services/apihero.server";
+import * as apihero from "~/services/apihero.server";
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
@@ -36,8 +36,6 @@ if (process.env.NODE_ENV === "production") {
   console.log("🚦 Sentry initialized");
 }
 
-// if (process.env.NODE_ENV === "development") {
-//   apihero.proxy.start(() => {
-//     console.info("🔶 API Hero proxy running");
-//   });
-// }
+apihero.proxy.start(() => {
+  console.info("🔶 API Hero proxy running");
+});
